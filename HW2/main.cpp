2d29f85b0732863a462820315f5fb66daaa6f109
Tiankai Ma <tiankaima@163.com>
2024-03-27 17:11:04 +0800

    feat: add HW2 structure

---
+++ HW2/main.cpp
@@ -0,0 +1,26 @@
+// Use svd to compress image
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <vector>
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+int main(int argc, char** argv)
+{
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <input_image_path> <output_image_path> <quality>" << std::endl;
+        return 1;
+    }
+
+    int width, height, channels;
+    unsigned char* img = stbi_load(argv[1], &width, &height, &channels, 0);
+    if (img == NULL) {
+        std::cerr << "Error: failed to load image" << std::endl;
+        return 1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# USTC Math Modeling course homework
+
+## [Homework1](./HW1/)
+
+利用 Python 实现 Seam carving 算法对图片进行有内容感知的缩放。
+
+## [Homework2](./HW2/)
+
+使用 C++ 实现 SVD 算法对图片进行有损压缩。
